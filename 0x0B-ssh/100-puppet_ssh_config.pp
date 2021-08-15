e55a842ff9138cfad5ea400daf7e9dd74fc7f0a7
anamariaroman <2979@holbertonschool.com>
2021-08-15 12:17:34 -0500

    task adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Now that you have successfully connected to your server, we would also like to join the party.
-# Add the SSH public key below to your server so that we can connect using the ubuntu user.
+# Add the SSH public key below to your server so that we can connect using the ubuntu user
 exec { 'ssh_config':
   path    => '/bin',
   command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
