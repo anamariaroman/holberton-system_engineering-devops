4236cae959a493e41949d081562af1cc8b4a1588
anamariaroman <anamariaromanvalencia@gmail.com>
2021-09-28 12:02:04 +0700

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,17 +8,19 @@ def recurse(subreddit, hot_list=[], after=""):
     """ Function query in API of reddit.com and return
     the number of subscribers.
     """
-    URL = 'https://www.reddit.com/r/{}/hot.json?limit={}&after={}'.format(
-        subreddit, LIMIT, after)
-    r = requests.get(URL, headers=HEADERS, allow_redirects=False)
-    if r.status_code >= 400:
-        return None
-    response_json = r.json()
-    if response_json['data']['children'][0]['kind'] == 't3':
-        for post in response_json['data']['children']:
-            hot_list.append(post['data']['title'])
-    after = response_json['data']['after']
-    if after is None:
-        return hot_list
+    URL = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, after)
+    response = requests.get(
+        url, allow_redirects=False, headers={'User-Agent':  'brian-95'})
+    if response.status_code == 200:
+        data_json = response.json()
+        for key_title in data_json['data']['children']:
+                if 'data' in key_title:
+                    hot_list.append(key_title['data']['title'])
+        after = data_json['data']['after']
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
-        return recurse(subreddit, hot_list, after)
+        return None
